22aa04a68e43639d4162936daa31476a30f473b4
Jeremy Cerise <jcerise06@gmail.com>
2014-01-29 00:17:50 -0500

    Intial Commit
    -Added bootstrap-sass gem
    -Added basic layout for app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Naglist::Application.config.secret_key_base = '95f05260d8837b574e00bf1cfdbfe37b383a7620bae99bb0ab94662cdcaf3db24acfd4e5ad9750aa44f7fd62ad15434f406c6b3c91fcbc66b4ab8f1e66be0cb0'
